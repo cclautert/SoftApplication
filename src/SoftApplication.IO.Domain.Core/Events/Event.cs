bcb2499606a6cfd34183df332a0d549d05ac542a
cristiano.c.lautert@gmail.com <cristiano.c.lautert@gmail.com>
2019-09-11 16:47:09 -0300

    [First-Commit]

---
+++ src/SoftApplication.IO.Domain.Core/Events/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftApplication.IO.Domain.Core.Events
+{
+    public abstract class Event : Message
+    {
+
+    }
+}
---
+++ src/SoftApplication.IO.Domain/Models/Events/EventoRegistradoEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftApplication.IO.Domain.Models.Events
+{
+    class EventoRegistradoEvent
+    {
+    }
+}
